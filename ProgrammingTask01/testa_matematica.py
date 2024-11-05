cde41fa85ead14b4fd30d567cdf8c047273894b0
Matheus Finger <matheusfingerramos@gmail.com>
2024-11-05 20:49:39 -0300

    Adicionando teste da função de média simples do subpacote de média dentro de matematica.estatistica

--- ProgrammingTask01/testa_matematica.py
+++ ProgrammingTask01/testa_matematica.py
@@ -7,6 +7,7 @@ def teste():
     print("1 + 2 = {}".format(aritmetica.soma(1, 2)))
     print("Área do círculo de raio 2 = {}".format(geometria.area_circulo(2)))
     print("Área do retângulo de base 4 e altura 10 = {}".format(geometria.area_retangulo(4, 10)))
+    print("Média de 5, 6, 10 e 21 = {}".format(media.media_simples([5, 6, 10, 21])))
 
 if __name__ == '__main__':
     teste()
